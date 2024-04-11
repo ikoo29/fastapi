902ee76386e60abe930db886581696f850a151b9
ikoo29 <166725592+ikoo29@users.noreply.github.com>
2024-04-11 23:51:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+@app.get('/hello')
+async def hello():
+    return{"message":"hello world!"}

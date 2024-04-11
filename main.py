2e4528aa1954ffd207488ec3a37751029941c0ce
ikoo29 <166725592+ikoo29@users.noreply.github.com>
2024-04-12 01:19:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def get_coordinates(ads: str = Query(...), num: str = Query(...)):
         "address": full_ads,
         "format": "json",
         "type": "road",
-        "key": key
+        "key": "6F8A7554-1FF8-33C3-83EC-2B74113F97BD"
     }
 
     async with httpx.AsyncClient() as client:
